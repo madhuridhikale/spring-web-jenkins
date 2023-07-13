df58586dff9d7476edf2745424f3e3ce123b77b3
madhuridhikale74 <dhikalemadhuri74@gmail.com>
2023-07-13 12:48:31 +0530

    new file

---
+++ src/main/java/com/element/SpringWebJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.element;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/element/SpringWebJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.element;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
